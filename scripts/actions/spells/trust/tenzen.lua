983dde12016575dfb5204f39cb638f22138a0ba9
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-01-22 16:19:48 -0800

    Update tenzen.lua

--- scripts/actions/spells/trust/tenzen.lua
+++ scripts/actions/spells/trust/tenzen.lua
@@ -28,6 +28,41 @@ spellObject.onMobSpawn = function(mob)
         ai.r.JA, ai.s.SPECIFIC, xi.ja.THIRD_EYE)
 
     mob:setTrustTPSkillSettings(ai.tp.CLOSER_UNTIL_TP, ai.s.HIGHEST, 1500)
+
+    
+	
+	local trustLevel	= mob:getMainLvl()
+	local strbonus		= trustLevel
+	local dexbonus		= trustLevel
+	local attbonus		= trustLevel * 2
+	local accbonus		= trustLevel * 2
+	local defbonus		= trustLevel
+	local mdefbonus		= trustLevel / 10
+	local mevabonus		= trustLevel
+	local dabonus		= trustLevel / 2
+	local tabonus		= trustLevel / 8
+	local sbbonus		= trustLevel * 0.4
+	local sbiibonus		= trustLevel * 0.4
+	local gearhaste		= trustLevel * 20-- 33 x 75 = 2475 = 24.75% gearhaste
+	local abilhaste		= trustLevel * 20-- 33 x 75 = 2475 = 24.75% abilityhaste
+	
+	mob:addMod(xi.mod.STR, strbonus)
+	mob:addMod(xi.mod.DEX, dexbonus)
+	mob:addMod(xi.mod.ATT, attbonus)
+	mob:addMod(xi.mod.ACC, accbonus)
+	mob:addMod(xi.mod.DEF, defbonus)
+    mob:addMod(xi.mod.MDEF, mdefbonus)
+    mob:addMod(xi.mod.MEVA, mevabonus)
+	mob:addMod(xi.mod.DOUBLE_ATTACK, dabonus)
+	mob:addMod(xi.mod.TRIPLE_ATTACK, tabonus)
+	mob:addMod(xi.mod.SUBTLE_BLOW, sbbonus)
+	mob:addMod(xi.mod.SUBTLE_BLOW_II, sbiibonus)
+	mob:addMod(xi.mod.ENMITY, -30)
+--	mob:addMod(xi.mod.REGAIN, 75)
+    mob:addMod(xi.mod.HASTE_MAGIC, 1000) -- 1000 = 10% Haste (Magic)
+    mob:addMod(xi.mod.HASTE_GEAR, gearhaste) -- 1000 = 10% Haste (gear)
+    mob:addMod(xi.mod.HASTE_ABILITY, abilhaste) -- 1000 = 10% Haste (ability)
+    
 end
 
 spellObject.onMobDespawn = function(mob)
