5e5ada4e961ff07460e0269e53b93ea372983b18
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-02-05 16:46:16 -0800

    Update qultada.lua

--- scripts/actions/spells/trust/qultada.lua
+++ scripts/actions/spells/trust/qultada.lua
@@ -17,6 +17,8 @@ spellObject.onMobSpawn = function(mob)
     mob:addSimpleGambit(ai.t.PARTY, ai.c.NOT_STATUS, xi.effect.CORSAIRS_ROLL, ai.r.JA, ai.s.SPECIFIC, xi.ja.CORSAIRS_ROLL)
     mob:addSimpleGambit(ai.t.PARTY, ai.c.NOT_STATUS, xi.effect.CHAOS_ROLL, ai.r.JA, ai.s.SPECIFIC, xi.ja.CHAOS_ROLL)
 
+    mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.VELOCITY_SHOT, ai.r.JA, ai.s.SPECIFIC, xi.ja.VELOCITY_SHOT)
+
     mob:addSimpleGambit(ai.t.TARGET, ai.c.ALWAYS, 0, ai.r.RATTACK, 0, 0, 7)
 
     -- Notable: Uses a balance of melee and ranged attacks.
