e3b7035a03e3dccca419e9ff10b2516c733083c7
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-02-06 19:04:23 -0800

    Update apururu_uc.lua

--- scripts/actions/spells/trust/apururu_uc.lua
+++ scripts/actions/spells/trust/apururu_uc.lua
@@ -45,12 +45,9 @@ spellObject.onMobSpawn = function(mob)
         mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 76, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.CURE_IV)
     elseif mlvl < 91 and mlvl > 30 then
         mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 76, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.CURE_III)
-    -- elseif mlvl < 31 and mlvl > 17 then
-    elseif mlvl < 31 and mlvl > 25 then
-        -- mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 76, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.CURE_II)
-        mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 76, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.CURE)
-    -- elseif mlvl < 18 then
-    elseif mlvl < 26 then
+    elseif mlvl < 31 and mlvl > 17 then
+        mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 76, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.CURE_II)
+    elseif mlvl < 18 then
         mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 76, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.CURE)
     end
     --  mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 75, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.CURE)
