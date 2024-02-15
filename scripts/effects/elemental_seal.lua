f9f1a81ac783e728356ab7313ea6f0f44be0b65f
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-02-14 20:32:54 -0800

    Update elemental_seal.lua

--- scripts/effects/elemental_seal.lua
+++ scripts/effects/elemental_seal.lua
@@ -6,7 +6,7 @@ local effectObject = {}
 effectObject.onEffectGain = function(target, effect)
 
     target:addMod(xi.mod.MAGIC_DAMAGE , 250)
-    target:addMod(xi.mod.MATT, 30)
+    target:addMod(xi.mod.MATT, 50)
     
 end
 
@@ -16,7 +16,7 @@ end
 effectObject.onEffectLose = function(target, effect)
 
     target:delMod(xi.mod.MAGIC_DAMAGE , 250)
-    target:delMod(xi.mod.MATT, 30)
+    target:delMod(xi.mod.MATT, 50)
     
 end
 
