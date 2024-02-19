8e10a1e36883af2e93cdd6a3ffe46c3e4d432597
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-02-19 00:19:53 -0800

    Update composure.lua

--- scripts/effects/composure.lua
+++ scripts/effects/composure.lua
@@ -9,8 +9,8 @@ effectObject.onEffectGain = function(target, effect)
     local jpValue = target:getJobPointLevel(xi.jp.COMPOSURE_EFFECT)
 
     target:addMod(xi.mod.ACC, 15 + jpValue)
-    -- target:addMod(xi.mod.MAGIC_DAMAGE , 150)
-    target:addMod(xi.mod.MATT, 330)
+    target:addMod(xi.mod.MAGIC_DAMAGE , 150)
+    target:addMod(xi.mod.MATT, 30)
     target:addMod(xi.mod.MACC, 50)
     target:addMod(xi.mod.FASTCAST, 25)
 end
@@ -22,8 +22,8 @@ effectObject.onEffectLose = function(target, effect)
     local jpValue = target:getJobPointLevel(xi.jp.COMPOSURE_EFFECT)
 
     target:delMod(xi.mod.ACC, 15 + jpValue)
-    -- target:delMod(xi.mod.MAGIC_DAMAGE , 150)
-    target:delMod(xi.mod.MATT, 330)
+    target:delMod(xi.mod.MAGIC_DAMAGE , 150)
+    target:delMod(xi.mod.MATT, 30)
     target:delMod(xi.mod.MACC, 50)
     target:delMod(xi.mod.FASTCAST, 25)
 end
