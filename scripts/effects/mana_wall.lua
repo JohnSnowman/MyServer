c103ebfaa00b4c081f5c1d628f93ed8388693b98
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-02-20 16:58:01 -0800

    Update mana_wall.lua

--- scripts/effects/mana_wall.lua
+++ scripts/effects/mana_wall.lua
@@ -4,14 +4,18 @@
 local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
-    target:addMod(xi.mod.DMG, 5000)
+    target:addMod(xi.mod.DMG, -5000)
+    target:addMod(xi.mod.UDMGPHYS, -2500)
+    target:addMod(xi.mod.UDMGMAGIC, -2500)
 end
 
 effectObject.onEffectTick = function(target, effect)
 end
 
 effectObject.onEffectLose = function(target, effect)
-    target:delMod(xi.mod.DMG, 5000)
+    target:delMod(xi.mod.DMG, -5000)
+    target:delMod(xi.mod.UDMGPHYS, -2500)
+    target:delMod(xi.mod.UDMGMAGIC, -2500)
 end
 
 return effectObject
