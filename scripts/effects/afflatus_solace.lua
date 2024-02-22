f6b3a08e79ac18b2a8432d6425873d52beb6e889
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-02-21 18:29:42 -0800

    Update afflatus_solace.lua

--- scripts/effects/afflatus_solace.lua
+++ scripts/effects/afflatus_solace.lua
@@ -6,6 +6,10 @@ local effectObject = {}
 effectObject.onEffectGain = function(target, effect)
     target:addMod(xi.mod.AFFLATUS_SOLACE, 0)
     target:addMod(xi.mod.BARSPELL_MDEF_BONUS, 5)
+    target:addMod(xi.mod.MAGIC_DAMAGE , 100)
+    target:addMod(xi.mod.MATT, 10)
+    target:addMod(xi.mod.MACC, 50)
+    target:addMod(xi.mod.FASTCAST, 25)
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -14,6 +18,10 @@ end
 effectObject.onEffectLose = function(target, effect)
     target:delMod(xi.mod.AFFLATUS_SOLACE, 0)
     target:delMod(xi.mod.BARSPELL_MDEF_BONUS, 5)
+    target:delMod(xi.mod.MAGIC_DAMAGE , 100)
+    target:delMod(xi.mod.MATT, 10)
+    target:delMod(xi.mod.MACC, 50)
+    target:delMod(xi.mod.FASTCAST, 25)
 end
 
 return effectObject
