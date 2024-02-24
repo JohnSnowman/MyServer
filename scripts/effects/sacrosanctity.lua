cdbdef05810c56adb552e55a8d4f38592a72a9ef
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-02-24 14:53:19 -0800

    Update sacrosanctity.lua

--- scripts/effects/sacrosanctity.lua
+++ scripts/effects/sacrosanctity.lua
@@ -5,6 +5,9 @@ local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
     target:addMod(xi.mod.MDEF, 75)
+    target:addMod(xi.mod.MEVA, 150)
+    target:addMod(xi.mod.DMG, -2500)
+    target:addMod(xi.mod.DMGMAGIC_II, -2500)
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -12,6 +15,9 @@ end
 
 effectObject.onEffectLose = function(target, effect)
     target:delMod(xi.mod.MDEF, 75)
+    target:delMod(xi.mod.MEVA, 150)
+    target:delMod(xi.mod.DMG, -2500)
+    target:delMod(xi.mod.DMGMAGIC_II, -2500)
 end
 
 return effectObject
