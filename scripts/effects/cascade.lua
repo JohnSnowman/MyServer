27cd9586f0f01550112e02db352deaa0cbb9e3e6
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-03-02 19:23:55 -0800

    Update cascade.lua

--- scripts/effects/cascade.lua
+++ scripts/effects/cascade.lua
@@ -5,8 +5,8 @@ local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
 
-    target:addMod(xi.mod.MAGIC_DAMAGE , 250)
-    target:addMod(xi.mod.MATT, 150)
+    target:addMod(xi.mod.MAGIC_DAMAGE , 200)
+    target:addMod(xi.mod.MATT, 200)
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -14,8 +14,8 @@ end
 
 effectObject.onEffectLose = function(target, effect)
 
-    target:delMod(xi.mod.MAGIC_DAMAGE , 250)
-    target:delMod(xi.mod.MATT, 150)
+    target:delMod(xi.mod.MAGIC_DAMAGE , 200)
+    target:delMod(xi.mod.MATT, 200)
 end
 
 return effectObject
