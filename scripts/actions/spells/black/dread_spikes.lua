16ccf207cec65750b3db74fcbfba5c3e98aed3ec
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-03-09 11:58:18 -0800

    Update dread_spikes.lua

--- scripts/actions/spells/black/dread_spikes.lua
+++ scripts/actions/spells/black/dread_spikes.lua
@@ -10,11 +10,12 @@ end
 spellObject.onSpellCast = function(caster, target, spell)
     local duration = calculateDuration(xi.settings.main.SPIKE_EFFECT_DURATION, spell:getSkillType(), spell:getSpellGroup(), caster, target)
     local typeEffect = xi.effect.DREAD_SPIKES
-    local drainAmount = target:getMaxHP() / 2
+    -- local drainAmount = target:getMaxHP() / 2
+    local drainAmount = target:getMaxHP() * 2
 
     drainAmount = drainAmount * (1 + (caster:getMod(xi.mod.DREAD_SPIKES_EFFECT) / 100))
 
-    if target:addStatusEffect(typeEffect, 0, 0, duration, 0, drainAmount, 1) then
+    if target:addStatusEffect(typeEffect, 0, 0, duration * 5, 0, drainAmount, 1) then
         spell:setMsg(xi.msg.basic.MAGIC_GAIN_EFFECT)
     else
         spell:setMsg(xi.msg.basic.MAGIC_NO_EFFECT)
