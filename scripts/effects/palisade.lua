3eb1b64e01e8ac7b63e790247c258c43e0801332
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-04-05 14:07:26 -0700

    Update palisade.lua

--- scripts/effects/palisade.lua
+++ scripts/effects/palisade.lua
@@ -5,6 +5,9 @@ local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
     target:addMod(xi.mod.PALISADE_BLOCK_BONUS, effect:getPower())
+    target:addMod(xi.mod.ENMITY, 100)
+    target:addMod(xi.mod.SHIELDBLOCKRATE, 30)
+    target:addMod(xi.mod.REPRISAL_BLOCK_BONUS, 10)
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -12,6 +15,9 @@ end
 
 effectObject.onEffectLose = function(target, effect)
     target:delMod(xi.mod.PALISADE_BLOCK_BONUS, effect:getPower())
+    target:delMod(xi.mod.ENMITY, 100)
+    target:delMod(xi.mod.SHIELDBLOCKRATE, 30)
+    target:delMod(xi.mod.REPRISAL_BLOCK_BONUS, 10)
 end
 
 return effectObject
