0d26ccb7af3712ed21ffcb1f93c84d63525ef943
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-04-09 13:03:13 -0700

    Update warding_circle.lua

--- scripts/effects/warding_circle.lua
+++ scripts/effects/warding_circle.lua
@@ -7,6 +7,10 @@ effectObject.onEffectGain = function(target, effect)
     local jpValue = target:getJobPointLevel(xi.jp.WARDING_CIRCLE_EFFECT)
 
     target:addMod(xi.mod.DEMON_KILLER, effect:getPower() + jpValue)
+    target:addMod(xi.mod.STORETP, 125)
+    target:addMod(xi.mod.HASTE_ABILITY, 1500)
+    target:addMod(xi.mod.WEAPONSKILL_DAMAGE_BASE, 25)
+    target:addMod(xi.mod.ALL_WSDMG_ALL_HITS, 100)
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -16,6 +20,10 @@ effectObject.onEffectLose = function(target, effect)
     local jpValue = target:getJobPointLevel(xi.jp.WARDING_CIRCLE_EFFECT)
 
     target:delMod(xi.mod.DEMON_KILLER, effect:getPower() + jpValue)
+    target:delMod(xi.mod.STORETP, 125)
+    target:delMod(xi.mod.HASTE_ABILITY, 1500)
+    target:delMod(xi.mod.WEAPONSKILL_DAMAGE_BASE, 25)
+    target:delMod(xi.mod.ALL_WSDMG_ALL_HITS, 100)
 end
 
 return effectObject
