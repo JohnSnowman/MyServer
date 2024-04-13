71038957a1c3d01ab97fc90e65ac1ee9353e4bea
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-04-13 16:22:52 -0700

    Update charentity.cpp

--- src/map/entities/charentity.cpp
+++ src/map/entities/charentity.cpp
@@ -1744,7 +1744,7 @@ void CCharEntity::OnRangedAttack(CRangeState& state, action_t& action)
         hitCount += getMod(Mod::UTSUSEMI);
     }
     // else if (this->StatusEffectContainer->HasStatusEffect(EFFECT_DOUBLE_SHOT) && xirand::GetRandomNumber(100) < (40 + this->getMod(Mod::DOUBLE_SHOT_RATE)))
-    else if xirand::GetRandomNumber(100) < (getMod(Mod::DOUBLE_SHOT_RATE))
+    else if xirand::GetRandomNumber(100) < (this->getMod(Mod::DOUBLE_SHOT_RATE))
     {
         hitCount = 2;
     }
