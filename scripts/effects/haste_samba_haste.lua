813a163b0add6f1ce25298ca670de7fd52ae7949
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-04-16 17:32:15 -0700

    Update haste_samba_haste.lua

--- scripts/effects/haste_samba_haste.lua
+++ scripts/effects/haste_samba_haste.lua
@@ -6,6 +6,15 @@ local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
     target:addMod(xi.mod.HASTE_ABILITY, effect:getPower())
+    
+    target:addMod(xi.mod.ATTP, 70)
+    target:addMod(xi.mod.DOUBLE_ATTACK, 15)
+    target:addMod(xi.mod.DOUBLE_ATTACK_DMG, 50)
+    target:addMod(xi.mod.MAIN_DMG_RATING, 15)
+    target:addMod(xi.mod.STORETP, 70)
+    target:addMod(xi.mod.WEAPONSKILL_DAMAGE_BASE, 15)
+    target:addMod(xi.mod.ALL_WSDMG_ALL_HITS, 50)
+    target:addMod(xi.mod.SAVETP, 250)
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -13,6 +22,15 @@ end
 
 effectObject.onEffectLose = function(target, effect)
     target:delMod(xi.mod.HASTE_ABILITY, effect:getPower())
+    
+    target:delMod(xi.mod.ATTP, 70)
+    target:delMod(xi.mod.DOUBLE_ATTACK, 15)
+    target:delMod(xi.mod.DOUBLE_ATTACK_DMG, 50)
+    target:delMod(xi.mod.MAIN_DMG_RATING, 15)
+    target:delMod(xi.mod.STORETP, 70)
+    target:delMod(xi.mod.WEAPONSKILL_DAMAGE_BASE, 15)
+    target:delMod(xi.mod.ALL_WSDMG_ALL_HITS, 50)
+    target:delMod(xi.mod.SAVETP, 250)
 end
 
 return effectObject
