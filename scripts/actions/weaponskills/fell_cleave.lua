a0ef5cd2bc5806fc4b4195cc17c227c53ccfdff2
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-04-28 15:11:06 -0700

    Update fell_cleave.lua

--- scripts/actions/weaponskills/fell_cleave.lua
+++ scripts/actions/weaponskills/fell_cleave.lua
@@ -24,7 +24,7 @@ weaponskillObject.onUseWeaponSkill = function(player, target, wsID, tp, primary,
     params.atk100 = 1 params.atk200 = 1 params.atk300 = 1
 
     if xi.settings.main.USE_ADOULIN_WEAPON_SKILL_CHANGES then
-        params.ftp100 = 2.75 params.ftp200 = 2.75 params.ftp300 = 2.75
+        params.ftp100 = 20.75 params.ftp200 = 20.75 params.ftp300 = 20.75
     end
 
     local damage, criticalHit, tpHits, extraHits = xi.weaponskills.doPhysicalWeaponskill(player, target, wsID, params, tp, action, primary, taChar)
