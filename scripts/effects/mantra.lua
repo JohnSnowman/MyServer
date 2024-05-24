ad8a570049dd85f87b5d4e746e261ab5595297c8
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-05-24 16:58:28 -0700

    Update mantra.lua

--- scripts/effects/mantra.lua
+++ scripts/effects/mantra.lua
@@ -1,1 +1,17 @@
+-----------------------------------
+-- xi.effect.MANTRA
+-----------------------------------
+local effectObject = {}
 
+effectObject.onEffectGain = function(target, effect)
+    target:addMod(xi.mod.HPP, 40)
+end
+
+effectObject.onEffectTick = function(target, effect)
+end
+
+effectObject.onEffectLose = function(target, effect)
+    target:delMod(xi.mod.HPP, 40)
+end
+
+return effectObject
