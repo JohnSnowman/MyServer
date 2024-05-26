1242577735ce6de029fb2e75240917a3cfbaefc5
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-05-26 09:42:10 -0700

    Update barrage.lua

--- scripts/effects/barrage.lua
+++ scripts/effects/barrage.lua
@@ -7,6 +7,7 @@ effectObject.onEffectGain = function(target, effect)
     local jpValue = target:getJobPointLevel(xi.jp.BARRAGE_EFFECT)
 
     target:addMod(xi.mod.RATT, jpValue * 3)
+    target:addMod(xi.mod.RANGED_DMG_RATING, 30)
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -16,6 +17,7 @@ effectObject.onEffectLose = function(target, effect)
     local jpValue = target:getJobPointLevel(xi.jp.BARRAGE_EFFECT)
 
     target:delMod(xi.mod.RATT, jpValue * 3)
+    target:delMod(xi.mod.RANGED_DMG_RATING, 30)
 end
 
 return effectObject
