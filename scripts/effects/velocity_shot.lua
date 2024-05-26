db5a2a56a4c45b8a41b76586435b4fa035c7ea36
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-05-26 09:41:45 -0700

    Update velocity_shot.lua

--- scripts/effects/velocity_shot.lua
+++ scripts/effects/velocity_shot.lua
@@ -7,10 +7,11 @@ effectObject.onEffectGain = function(target, effect)
     local jpValue = target:getJobPointLevel(xi.jp.VELOCITY_SHOT_EFFECT)
 
     target:addMod(xi.mod.RATT, jpValue * 2)
-    target:addMod(xi.mod.ATTP, -40)
+    target:addMod(xi.mod.ATTP, -50)
     target:addMod(xi.mod.HASTE_ABILITY, -1500)
-    target:addMod(xi.mod.RATTP, 40)
-    target:addMod(xi.mod.RANGED_DELAYP, -30)
+    target:addMod(xi.mod.RATTP, 50)
+    target:addMod(xi.mod.RANGED_DELAYP, -50)
+    target:addMod(xi.mod.RANGED_DMG_RATING, 30)
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -20,10 +21,11 @@ effectObject.onEffectLose = function(target, effect)
     local jpValue = target:getJobPointLevel(xi.jp.VELOCITY_SHOT_EFFECT)
 
     target:delMod(xi.mod.RATT, jpValue * 2)
-    target:delMod(xi.mod.ATTP, -40)
+    target:delMod(xi.mod.ATTP, -50)
     target:delMod(xi.mod.HASTE_ABILITY, -1500)
-    target:delMod(xi.mod.RATTP, 40)
-    target:delMod(xi.mod.RANGED_DELAYP, -30)
+    target:delMod(xi.mod.RATTP, 50)
+    target:delMod(xi.mod.RANGED_DELAYP, -50)
+    target:delMod(xi.mod.RANGED_DMG_RATING, 30)
 end
 
 return effectObject
