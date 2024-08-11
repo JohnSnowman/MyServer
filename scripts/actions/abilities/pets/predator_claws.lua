ad54465155463c748e8d9a408cf12368e84d3c8b
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-08-10 22:42:59 -0700

    Update predator_claws.lua

--- scripts/actions/abilities/pets/predator_claws.lua
+++ scripts/actions/abilities/pets/predator_claws.lua
@@ -11,7 +11,7 @@ abilityObject.onPetAbility = function(target, pet, skill)
     local numhits = 3
     local accmod = 1
     local dmgmod = 10
-    local dmgmodsubsequent = 2
+    local dmgmodsubsequent = 10
     local damage = xi.summon.avatarPhysicalMove(pet, target, skill, numhits, accmod, dmgmod, dmgmodsubsequent, xi.mobskills.magicalTpBonus.NO_EFFECT, 1, 2, 3)
     local totaldamage = xi.summon.avatarFinalAdjustments(damage.dmg, pet, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, numhits)
     target:takeDamage(totaldamage, pet, xi.attackType.PHYSICAL, xi.damageType.SLASHING)
