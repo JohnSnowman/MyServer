356e51f68e5c8abfad1c5474690413fa1739ff7a
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-11-03 13:00:23 -0800

    Update rune_fencer.lua

--- scripts/globals/job_utils/rune_fencer.lua
+++ scripts/globals/job_utils/rune_fencer.lua
@@ -22,7 +22,7 @@ end
 
 local function applyRuneEnhancement(effectType, player)
     local runLevel      = getRUNLevel(player)
-    local meritBonus    = 0--player:getMerit(xi.merit.MERIT_RUNE_ENHANCE) -- 2 more elemental resistance per merit for a maximum total of (2*5) = 10 (power of merit is 2 per level)
+    local meritBonus    = player:getMerit(xi.merit.MERIT_RUNE_ENHANCE) -- 2 more elemental resistance per merit for a maximum total of (2*5) = 10 (power of merit is 2 per level)
     local jobPointBonus = player:getJobPointLevel(xi.jp.RUNE_ENCHANTMENT_EFFECT) -- 1 more elemental resistance per level for a maximum total of 20
 
     -- see https://www.bg-wiki.com/ffxi/Category:Rune
