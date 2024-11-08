eeabdcf7150888260097dd768398fa6b717ee583
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-11-07 22:41:44 -0800

    Update sentinel.lua

--- scripts/actions/abilities/sentinel.lua
+++ scripts/actions/abilities/sentinel.lua
@@ -8,7 +8,10 @@
 local abilityObject = {}
 
 abilityObject.onAbilityCheck = function(player, target, ability)
-    return xi.job_utils.paladin.checkSentinel(player, target, ability)
+    ability:setRecast(ability:getRecast() - 300)
+    --ability:setRecast(math.max(0, ability:getRecast() - player:getMod(xi.mod.CALL_BEAST_DELAY)))
+    --ability:setRecast(ability:getRecast() - jpValue)
+    return 0, 0
 end
 
 abilityObject.onUseAbility = function(player, target, ability)
