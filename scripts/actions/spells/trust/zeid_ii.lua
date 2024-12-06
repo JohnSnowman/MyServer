f923a8f3570a985c195f260c296b2cac2dc8e9af
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-12-06 12:34:57 -0800

    Update zeid_ii.lua

--- scripts/actions/spells/trust/zeid_ii.lua
+++ scripts/actions/spells/trust/zeid_ii.lua
@@ -22,28 +22,70 @@ spellObject.onMobSpawn = function(mob)
             xi.trust.message(mobArg, xi.trust.messageOffset.SPECIAL_MOVE_1)
         end
     end)
-
+    
+    local mlvl = mob:getMainLvl()
+    if mlvl > 4 then
+    mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.ARCANE_CIRCLE, ai.r.JA, ai.s.SPECIFIC, xi.ja.ARCANE_CIRCLE)
+    end
     -- Stun all the things!
-    mob:addSimpleGambit(ai.t.TARGET, ai.c.READYING_WS, 0,
-                        ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.STUN)
+    mob:addSimpleGambit(ai.t.TARGET, ai.c.READYING_WS, 0, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.STUN)
 
-    mob:addSimpleGambit(ai.t.TARGET, ai.c.READYING_MS, 0,
-                        ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.STUN)
+    mob:addSimpleGambit(ai.t.TARGET, ai.c.READYING_MS, 0, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.STUN)
 
-    mob:addSimpleGambit(ai.t.TARGET, ai.c.READYING_JA, 0,
-                        ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.STUN)
+    mob:addSimpleGambit(ai.t.TARGET, ai.c.READYING_JA, 0, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.STUN)
 
-    mob:addSimpleGambit(ai.t.TARGET, ai.c.CASTING_MA, 0,
-                        ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.STUN)
+    mob:addSimpleGambit(ai.t.TARGET, ai.c.CASTING_MA, 0, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.STUN)
 
     -- Non-stun things
-    mob:addSimpleGambit(ai.t.SELF, ai.c.ALWAYS, 0,
-                        ai.r.JA, ai.s.SPECIFIC, xi.ja.SOULEATER)
+    if mlvl > 29 then
+    mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.SOULEATER, ai.r.JA, ai.s.SPECIFIC, xi.ja.SOULEATER)
+    end
+    
+    if mlvl > 14 then
+    mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.LAST_RESORT, ai.r.JA, ai.s.SPECIFIC, xi.ja.LAST_RESORT)
+    end
+    
+    mob:setTrustTPSkillSettings(ai.tp.CLOSER_UNTIL_TP, ai.s.RANDOM, 1250)
 
-    mob:addSimpleGambit(ai.t.SELF, ai.c.ALWAYS, 0,
-                        ai.r.JA, ai.s.SPECIFIC, xi.ja.LAST_RESORT)
+    
+	
+	local trustLevel	= mob:getMainLvl()
+	local strbonus		= trustLevel
+	local dexbonus		= trustLevel
+	local attbonus		= trustLevel * 2
+	local accbonus		= trustLevel * 2
+	local defbonus		= trustLevel
+	local mdefbonus		= trustLevel / 10
+	local mevabonus		= trustLevel
+	local dabonus		= trustLevel / 3
+	local tabonus		= trustLevel / 10
+	local stpbonus		= trustLevel / 2
+	local sbbonus		= trustLevel * 0.4
+	local sbiibonus		= trustLevel * 0.4
+	local magichaste	= trustLevel * 10-- 33 x 75 = 2475 = 24.75% gearhaste
+	local gearhaste		= trustLevel * 10-- 33 x 75 = 2475 = 24.75% gearhaste
+	local abilhaste		= trustLevel * 10-- 33 x 75 = 2475 = 24.75% abilityhaste
+	
+	mob:addMod(xi.mod.STR, strbonus)
+	mob:addMod(xi.mod.DEX, dexbonus)
+	mob:addMod(xi.mod.ATT, attbonus)
+	mob:addMod(xi.mod.ACC, accbonus)
+	mob:addMod(xi.mod.DEF, defbonus)
+    mob:addMod(xi.mod.MDEF, mdefbonus)
+    mob:addMod(xi.mod.MEVA, mevabonus)
+	mob:addMod(xi.mod.DOUBLE_ATTACK, dabonus)
+	mob:addMod(xi.mod.TRIPLE_ATTACK, tabonus)
+	mob:addMod(xi.mod.STORETP, stpbonus)
+	mob:addMod(xi.mod.SUBTLE_BLOW, sbbonus)
+	mob:addMod(xi.mod.SUBTLE_BLOW_II, sbiibonus)
+	mob:addMod(xi.mod.ENMITY, -30)
+--	mob:addMod(xi.mod.REGAIN, 75)
+    mob:addMod(xi.mod.HASTE_MAGIC, magichaste) -- 1000 = 10% Haste (Magic)
+    mob:addMod(xi.mod.HASTE_GEAR, gearhaste) -- 1000 = 10% Haste (gear)
+    mob:addMod(xi.mod.HASTE_ABILITY, abilhaste) -- 1000 = 10% Haste (ability)
 
-    mob:setTrustTPSkillSettings(ai.tp.CLOSER_UNTIL_TP, ai.s.RANDOM, 3000)
+    -- Movement
+	mob:addMod(xi.mod.MOVE_SPEED_OVERIDE, 250)
 end
 
 spellObject.onMobDespawn = function(mob)
