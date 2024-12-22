abb612ea882762e811290cb87df611fed4d1bb15
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-12-21 16:46:56 -0800

    Update endark_ii.lua

--- scripts/actions/spells/black/endark_ii.lua
+++ scripts/actions/spells/black/endark_ii.lua
@@ -8,15 +8,20 @@ spellObject.onMagicCastingCheck = function(caster, target, spell)
 end
 
 spellObject.onSpellCast = function(caster, target, spell)
-    local effect = xi.effect.ENDARK_II
-    local magicskill = target:getSkillLevel(xi.skill.DARK_MAGIC)
-    local potency = (magicskill / 2) + 12.5
+    
+    target:addStatusEffect(xi.effect.BLOOD_WEAPON, 1, 0, 3600)
 
-    if target:addStatusEffect(effect, potency, 0, 3600) then
-        spell:setMsg(xi.msg.basic.MAGIC_GAIN_EFFECT)
-    else
-        spell:setMsg(xi.msg.basic.MAGIC_NO_EFFECT)
-    end
+    spell:setMsg(xi.msg.basic.MAGIC_GAIN_EFFECT)
+    
+    --local effect = xi.effect.ENDARK_II
+    --local magicskill = target:getSkillLevel(xi.skill.DARK_MAGIC)
+    --local potency = (magicskill / 2) + 12.5
+
+    --if target:addStatusEffect(effect, potency, 0, 3600) then
+    --    spell:setMsg(xi.msg.basic.MAGIC_GAIN_EFFECT)
+    --else
+    --    spell:setMsg(xi.msg.basic.MAGIC_NO_EFFECT)
+    --end
 
     return effect
 end
