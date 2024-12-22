6b1bbcf260b785962cf3ffd516a594e30bc0ae6c
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-12-21 16:24:38 -0800

    Create endark_ii.lua

---
+++ scripts/actions/spells/black/endark_ii.lua
@@ -0,0 +1,24 @@
+-----------------------------------
+-- Spell: Endark_II
+-----------------------------------
+local spellObject = {}
+
+spellObject.onMagicCastingCheck = function(caster, target, spell)
+    return 0
+end
+
+spellObject.onSpellCast = function(caster, target, spell)
+    local effect = xi.effect.ENDARK
+    local magicskill = target:getSkillLevel(xi.skill.DARK_MAGIC)
+    local potency = (magicskill / 2) + 12.5
+
+    if target:addStatusEffect(effect, potency, 0, 3600) then
+        spell:setMsg(xi.msg.basic.MAGIC_GAIN_EFFECT)
+    else
+        spell:setMsg(xi.msg.basic.MAGIC_NO_EFFECT)
+    end
+
+    return effect
+end
+
+return spellObject
