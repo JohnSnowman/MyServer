0305e5920f73febdd63ef7c63505dbe8ae69a6d7
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-12-23 13:22:49 -0800

    Update endark.lua

--- scripts/actions/spells/black/endark.lua
+++ scripts/actions/spells/black/endark.lua
@@ -10,7 +10,7 @@ end
 spellObject.onSpellCast = function(caster, target, spell)
     local effect = xi.effect.ENDARK
     --local magicskill = target:getSkillLevel(xi.skill.DARK_MAGIC)
-    local potency = 2
+    local potency = .1
 
     if target:addStatusEffect(effect, potency, 0, 1800) then
         spell:setMsg(xi.msg.basic.MAGIC_GAIN_EFFECT)
