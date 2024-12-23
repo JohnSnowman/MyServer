5ebeee9380febf82482a39bdbba0a80eeb6e7348
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2024-12-23 13:53:45 -0800

    Update enlight.lua

--- scripts/effects/enlight.lua
+++ scripts/effects/enlight.lua
@@ -9,7 +9,8 @@ effectObject.onEffectGain = function(target, effect)
     target:addMod(xi.mod.ENSPELL, xi.element.LIGHT)
     target:addMod(xi.mod.ENSPELL_DMG, effect:getPower() + jpValue)
     target:addMod(xi.mod.ACC, jpValue)
-    target:addEnmity(player, 450, 900)
+    target:addEnmity(player, 3000, 3000)
+    --target:addEnmity(player, 450, 900)
 end
 
 effectObject.onEffectTick = function(target, effect)
