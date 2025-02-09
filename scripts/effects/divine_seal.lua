239150ea50acc60d63bdfd5775b1107df4eca1c5
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2025-02-08 16:11:59 -0800

    Update divine_seal.lua

--- scripts/effects/divine_seal.lua
+++ scripts/effects/divine_seal.lua
@@ -10,6 +10,7 @@ effectObject.onEffectGain = function(target, effect)
     target:addMod(xi.mod.MACC, 50)
     target:addMod(xi.mod.CURE_POTENCY, 50)
     target:addMod(xi.mod.CURE_POTENCY_II, 40)
+    target:addMod(xi.mod.CURE_POTENCY_BONUS, 300)
     target:addMod(xi.mod.FASTCAST, 25)
 end
 
@@ -23,6 +24,7 @@ effectObject.onEffectLose = function(target, effect)
     target:delMod(xi.mod.MACC, 50)
     target:delMod(xi.mod.CURE_POTENCY, 50)
     target:delMod(xi.mod.CURE_POTENCY_II, 40)
+    target:delMod(xi.mod.CURE_POTENCY_BONUS, 300)
     target:delMod(xi.mod.FASTCAST, 25)
 end
 
