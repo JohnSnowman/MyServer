282bcd5c7cb0fe289112132c6fe0f2efbfd0306b
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2025-02-10 16:33:36 -0800

    Update mihli_aliapoh.lua

--- scripts/actions/spells/trust/mihli_aliapoh.lua
+++ scripts/actions/spells/trust/mihli_aliapoh.lua
@@ -24,15 +24,23 @@ spellObject.onMobSpawn = function(mob)
         [xi.magic.spell.ZAZARG] = xi.trust.messageOffset.TEAMWORK_4,
     })
 
-    mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 25, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.CURE)
+    local mlvl = mob:getMainLvl()
+    if mlvl > 14 then
+	mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.DIVINE_SEAL, ai.r.JA, ai.s.SPECIFIC, xi.ja.DIVINE_SEAL)
+    elseif mlvl > 29 then
+    mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.BERSERK, ai.r.JA, ai.s.SPECIFIC, xi.ja.BERSERK)
+    elseif mlvl > 39 then
+	mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.AFFLATUS_SOLACE, ai.r.JA, ai.s.SPECIFIC, xi.ja.AFFLATUS_SOLACE)
+    end
+
+    mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 30, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.CURE)
+    --mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 25, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.CURE)
 
     mob:addSimpleGambit(ai.t.PARTY, ai.c.STATUS, xi.effect.SLEEP_I, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.CURE)
     mob:addSimpleGambit(ai.t.PARTY, ai.c.STATUS, xi.effect.SLEEP_II, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.CURE)
 
-    mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.AFFLATUS_SOLACE,
-                        ai.r.JA, ai.s.SPECIFIC, xi.ja.AFFLATUS_SOLACE)
-
-    mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 75, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.CURE)
+    mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 70, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.CURE)
+    --mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 75, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.CURE)
 
     mob:addSimpleGambit(ai.t.PARTY, ai.c.NOT_STATUS, xi.effect.PROTECT, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.PROTECTRA)
     mob:addSimpleGambit(ai.t.PARTY, ai.c.NOT_STATUS, xi.effect.SHELL, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.SHELLRA)
@@ -50,6 +58,8 @@ spellObject.onMobSpawn = function(mob)
 
     mob:addSimpleGambit(ai.t.TARGET, ai.c.NOT_STATUS, xi.effect.PARALYSIS, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.PARALYZE, 60)
     mob:addSimpleGambit(ai.t.TARGET, ai.c.NOT_STATUS, xi.effect.SLOW, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.SLOW, 60)
+    
+    mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 90, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.CURE)
 
     mob:addListener('WEAPONSKILL_USE', 'MIHLI_ALIAPOH_WEAPONSKILL_USE', function(mobArg, target, wsid, tp, action)
         if wsid == 3203 then -- Scouring Bubbles
@@ -59,6 +69,38 @@ spellObject.onMobSpawn = function(mob)
             end
         end
     end)
+
+    local trustLevel	= mob:getMainLvl()
+	local potencyi		= trustLevel / 5
+	local potencyii		= trustLevel / 8
+	local cureamount	= trustLevel * 2
+	local affsol		= trustLevel / 5
+	local enhdur		= trustLevel * 2
+	local castingspeed	= trustLevel / 4
+	local refreshmp		= trustLevel / 15
+	local mndbonus		= trustLevel
+	local mdefbonus		= trustLevel / 9
+	local mevabonus		= trustLevel * 3
+	local dmgtakenbon	= trustLevel * 15-- 35 time 75 = 2625 = 26.245%		Damage - 10000 base, 375 = 3.75%
+	
+	mob:addMod(xi.mod.CURE_POTENCY, potencyi)
+	mob:addMod(xi.mod.CURE_POTENCY_II, potencyii)
+	mob:addMod(xi.mod.CURE_POTENCY_BONUS, cureamount)
+	mob:addMod(xi.mod.AFFLATUS_SOLACE, affsol)
+	mob:addMod(xi.mod.ENH_MAGIC_DURATION, enhdur)
+	mob:addMod(xi.mod.FASTCAST, castingspeed)
+	mob:addMod(xi.mod.REFRESH, refreshmp)
+	mob:addMod(xi.mod.MND, mndbonus)
+    mob:addMod(xi.mod.MDEF, mdefbonus)
+    mob:addMod(xi.mod.MEVA, mevabonus)
+    mob:addMod(xi.mod.DMG, -dmgtakenbon)
+    mob:addMod(xi.mod.SLEEP_MEVA, 50)
+    mob:addMod(xi.mod.SILENCE_MEVA, 50)
+	mob:addMod(xi.mod.ENMITY, -50)
+
+    -- Movement
+	mob:addMod(xi.mod.MOVE_SPEED_OVERIDE, 250)
+    
 end
 
 spellObject.onMobDespawn = function(mob)
