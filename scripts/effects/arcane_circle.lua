210481a839451626517f519b879b4562a64bc6aa
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2025-06-27 10:28:43 -0700

    Update arcane_circle.lua

--- scripts/effects/arcane_circle.lua
+++ scripts/effects/arcane_circle.lua
@@ -13,6 +13,8 @@ effectObject.onEffectGain = function(target, effect)
     target:addMod(xi.mod.DOUBLE_ATTACK_DMG, 100)
     target:addMod(xi.mod.MAIN_DMG_RATING, 30)
     target:addMod(xi.mod.RANGED_DMG_RATING, 30)
+    target:addMod(xi.mod.MAGIC_DAMAGE , 300)
+    target:addMod(xi.mod.MATT, 200)
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -28,6 +30,8 @@ effectObject.onEffectLose = function(target, effect)
     target:delMod(xi.mod.DOUBLE_ATTACK_DMG, 100)
     target:delMod(xi.mod.MAIN_DMG_RATING, 30)
     target:delMod(xi.mod.RANGED_DMG_RATING, 30)
+    target:delMod(xi.mod.MAGIC_DAMAGE , 300)
+    target:delMod(xi.mod.MATT, 200)
 end
 
 return effectObject
