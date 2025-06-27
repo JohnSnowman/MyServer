c9409bbd504ae4e339eadd58cb1ba18b901e7e9c
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2025-06-27 09:42:30 -0700

    Update arcane_circle.lua

--- scripts/effects/arcane_circle.lua
+++ scripts/effects/arcane_circle.lua
@@ -5,6 +5,8 @@ local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
     target:addMod(xi.mod.ARCANA_KILLER, effect:getPower())
+    target:addMod(xi.mod.HPP, 50)
+    target:addMod(xi.mod.MPP, 100)
     target:addMod(xi.mod.ATTP, 125)
     target:addMod(xi.mod.HASTE_ABILITY, 1500)
     target:addMod(xi.mod.DOUBLE_ATTACK, 25)
@@ -18,6 +20,8 @@ end
 
 effectObject.onEffectLose = function(target, effect)
     target:delMod(xi.mod.ARCANA_KILLER, effect:getPower())
+    target:delMod(xi.mod.HPP, 50)
+    target:delMod(xi.mod.MPP, 100)
     target:delMod(xi.mod.ATTP, 125)
     target:delMod(xi.mod.HASTE_ABILITY, 1500)
     target:delMod(xi.mod.DOUBLE_ATTACK, 25)
