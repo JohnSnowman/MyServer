0c5464003345692ee9575a06593ac4d97407a33b
JohnSnowman <147958715+JohnSnowman@users.noreply.github.com>
2025-09-30 17:45:08 -0700

    Update attack.cpp

--- src/map/attack.cpp
+++ src/map/attack.cpp
@@ -587,7 +587,8 @@ void CAttack::ProcessDamage()
         {
             //puppetutils::TrySkillUP((CAutomatonEntity*)m_attacker, SKILL_AUTOMATON_MELEE, m_victim->GetMLevel());
             //charutils::TrySkillUP(PMaster, SKILL_SUMMONING_MAGIC, PMaster->GetMLevel());
-            charutils::TrySkillUP(m_attacker, SKILL_SUMMONING_MAGIC, m_victim->GetMLevel());
+            //charutils::TrySkillUP(m_attacker, SKILL_SUMMONING_MAGIC, m_victim->GetMLevel());
+            charutils::TrySkillUP((CCharEntity*)m_attacker, SKILLTYPE::SKILL_SUMMONING_MAGIC, m_victim->GetMLevel());
         }
     }
     m_isBlocked = attackutils::IsBlocked(m_attacker, m_victim);
